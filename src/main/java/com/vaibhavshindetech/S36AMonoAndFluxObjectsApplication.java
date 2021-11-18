8e657e1c72018e118b6a15b8baa59b1adeb24f65
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-18 09:27:35 +0530

    created mono and flux objects demo project

---
+++ src/main/java/com/vaibhavshindetech/S36AMonoAndFluxObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhavshindetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S36AMonoAndFluxObjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S36AMonoAndFluxObjectsApplication.class, args);
+	}
+
+}
